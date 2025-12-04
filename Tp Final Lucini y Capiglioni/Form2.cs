724c5e38587696318f191576273080acb741b9db
klpFalros05 <franlucini05@gmail.com>
2025-12-04 13:10:47 -0300

    Creacion de los forms para armarlos

---
+++ Tp Final Lucini y Capiglioni/Form2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tp_Final_Lucini_y_Capiglioni
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+    }
+}
